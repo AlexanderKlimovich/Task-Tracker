119c8108a8ca69a5ec736172edde80ed09bf59da
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-09-01 15:42:27 +0300

    add services for all models

--- src/main/java/com/klymovych/tasktracker/repository/UserRepository.java
+++ src/main/java/com/klymovych/tasktracker/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.klymovych.tasktracker.repository;
 
+import com.klymovych.tasktracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
